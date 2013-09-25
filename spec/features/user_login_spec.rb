eb5ea9b0ba966c28bc607fef5c77cebec0003041
Zach Young <zachjyoung@gmail.com>
2013-09-25 18:46:36 -0400

    sign up sign out register all works

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+feature 'User can login to their account', %Q{
+  As a User
+  I want to login to my account 
+  So I can review movies and vote on things
+}do 
+
+let(:user) { FactoryGirl.create(:user) }
+
+scenario 'user logs into their account from the front page' do 
+  visit '/'
+  click_on 'Sign in'
+  fill_in 'Email', with: user.email
+  fill_in 'Password', with: user.password
+  click_button 'Sign in'
+  expect(page).to have_content('Signed in successfully.')
+  end
+
+scenario 'user is a dummy and does not have the correct password' do 
+  visit '/'
+  click_on 'Sign in'
+  click_button 'Sign in'
+  expect(page).to have_content("Invalid email or password")
+  end
+
+scenario 'user logs out of site' do 
+  visit '/'
+  click_on 'Sign in'
+  fill_in 'Email', with: user.email
+  fill_in 'Password', with: user.password
+  click_button 'Sign in'
+  expect(page).to have_content('Signed in successfully.')
+  click_on 'Sign out'
+  expect(page).to have_content('Signed out successfully.')
+  end  
+end
--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
-
-
 feature 'User can register a account', %Q{
   As a User
   I want to register 
