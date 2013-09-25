129140e34a764376744cd30342e01e65714ebfc7
Zach Young <zachjyoung@gmail.com>
2013-09-25 15:39:08 -0400

    please dont be broken sad panda

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Letsgo2themovies::Application.config.secret_key_base = 'cdd8e8c74c5740e6703f7506f79f37b48ddf8c51d9950b389851e0c728a56be8f24bc1154762a681c796fc9b4c431a60822d814fbc5eff3e257d1b6333cc59ce'
