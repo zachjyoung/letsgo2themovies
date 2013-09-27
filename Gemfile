d38ae3ecfd210c58d4b11e232b9ccedf441d8a31
Zach Young <zachjyoung@gmail.com>
2013-09-27 13:34:38 -0400

    initial commit gem added carrierwave

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ group :development, :test do
 end
 
 gem 'devise'
+gem 'carrierwave'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Letsgo2themovies::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'home#index'
+    
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
