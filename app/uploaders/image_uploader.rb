84dd0b2b4e47f22817b6538c82d38cb303108a5b
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-09-30 18:37:13 -0400

    carrierwave actually works now, test still broken, sorry

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -28,12 +28,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # process :scale => [200, 300]
   #
   def scale(width, height)
-    # do something
+
   end
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :scale => [50, 50]
+    process :resize_to_fit => [100, 100]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
