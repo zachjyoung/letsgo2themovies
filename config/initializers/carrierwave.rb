f1832438000607321c668048f58569675fcb93f4
Zach Young <zachjyoung@gmail.com>
2013-10-01 15:29:28 -0400

    trying s3 please dont break

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,26 @@
+# config/initializers/carrierwave.rb
+CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => 'AKIAJF5R5ZHTZLKSJORA',                        # required
+    :aws_secret_access_key  => '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',                     # required
+    :region                 => 'us-east-1',                  # optional, defaults to 'us-east-1'
+    :host                   => 's3.example.com',             # optional, defaults to nil
+    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'directory'                             # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
+
+
 if Rails.env.test? or Rails.env.cucumber?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
   end
 end
+
