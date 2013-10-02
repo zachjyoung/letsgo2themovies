33aa9be84525d4557a5471d395089d3d4c9b6700
Zach Young <zachjyoung@gmail.com>
2013-10-02 14:37:13 -0400

    created new work commit please start from here

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :image, ImageUploader
-<<<<<<< HEAD
+
   has_many :authentications
   has_many :reviews
-=======
->>>>>>> parent of a62d534... github login works not twitter or facebook
+
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -231,16 +231,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  config.omniauth :github, '827f503bf52336f8a9f8', '62651b4661f5b6df18e670bdb300cd92edb57596', :scope => 'user,public_repo'
-=======
-  config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
->>>>>>> parent of 45761e9... bundled omniauth preparing next stage
-  config.omniauth :twitter, ENV["eOKmIsYnQFfLfuJj7J78yg"], ENV["Q0TJcJF0J6kNQqAfAWDuO9cUPp5eJDhghC441U71g"]
-  config.omniauth :facebook, 'APP_ID', 'APP_SECRET' 
->>>>>>> parent of a62d534... github login works not twitter or facebook
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
