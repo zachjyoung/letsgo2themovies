4ff2c6840611fed5228ab7353598c6d206cab5c0
Zach Young <zachjyoung@gmail.com>
2013-10-03 13:16:59 -0400

    movie image works when movie is added

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-
-ActiveRecord::Schema.define(version: 20130930225113) do
-
-=======
-ActiveRecord::Schema.define(version: 20130930225113) do
->>>>>>> parent of a62d534... github login works not twitter or facebook
+ActiveRecord::Schema.define(version: 20131003152359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +24,7 @@ ActiveRecord::Schema.define(version: 20130930225113) do
     t.string   "cast",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "reviews", force: true do |t|
@@ -41,7 +36,7 @@ ActiveRecord::Schema.define(version: 20130930225113) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -54,6 +49,9 @@ ActiveRecord::Schema.define(version: 20130930225113) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
